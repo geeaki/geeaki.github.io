1e135f0845ef78415cc964ecf3e0cb89abedf072
deeeki <deeeki@gmail.com>
2013-12-15 18:27:20 +0900

    Implement page rendering with markdown

--- app.rb
+++ app.rb
@@ -3,8 +3,20 @@ $:.unshift File.dirname(__FILE__)
 require 'bundler/setup'
 Bundler.require(:default)
 
+helpers do
+  def pages_path
+    "#{settings.root}/pages"
+  end
+
+  def markdown markdown_text
+    renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML, fenced_code_blocks: true)
+    renderer.render(markdown_text)
+  end
+end
+
 get '/index.html' do
-  slim :index
+  path = "#{pages_path}/index.md"
+  slim :index, locals: { page: File.read(path) }
 end
 
 get '/stylesheets/style.css' do
