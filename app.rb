a5f43ce8ed2f9034699172ef3ceb0bb90a714197
deeeki <deeeki@gmail.com>
2013-12-15 17:30:27 +0900

    Initialize stylesheets

--- app.rb
+++ app.rb
@@ -6,3 +6,7 @@ Bundler.require(:default)
 get '/index.html' do
   slim :index
 end
+
+get '/stylesheets/style.css' do
+  scss :style, views: 'stylesheets'
+end
--- spec/site_spec.rb
+++ spec/site_spec.rb
@@ -8,5 +8,6 @@ describe 'This site' do
 
   it do
     get '/index.html'
+    get '/stylesheets/style.css'
   end
 end
