84f548b445deef16eb2812b04aff73bc165d86d4
deeeki <deeeki@gmail.com>
2014-01-07 01:20:14 +0900

    Implement events page

--- app.rb
+++ app.rb
@@ -2,10 +2,15 @@ $:.unshift File.dirname(__FILE__)
 
 require 'bundler/setup'
 Bundler.require(:default)
+require 'yaml'
 
 use Rack::Static, :urls => ['/images']
 
 helpers do
+  def config_path
+    "#{settings.root}/config"
+  end
+
   def pages_path
     "#{settings.root}/pages"
   end
@@ -19,6 +24,11 @@ end
 get '/index.html' do
   path = "#{pages_path}/index.md"
   slim :index, locals: { page: File.read(path) }
+end
+
+get '/events/index.html' do
+  hash_events = YAML.load_file("#{config_path}/events.yml")
+  slim :events, locals: { events: hash_events.map{|e| OpenStruct.new(e) } }
 end
 
 get '/stylesheets/style.css' do
--- spec/site_spec.rb
+++ spec/site_spec.rb
@@ -8,6 +8,7 @@ describe 'This site' do
 
   it do
     get '/index.html'
+    get '/events/index.html'
     get '/stylesheets/style.css'
     get '/images/favicon.ico'
     get '/images/geeaki_living.jpg'
